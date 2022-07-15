ea6b1d77a4b184437d9c403087871e39ec6b2812
amir <babaamir325@gmail.com>
2022-07-15 22:47:39 +0530

    state added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './componets/Navbar';
+import TextForm from './componets/TextForm';
+import React, {useState} from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () =>{
+     if(mode === 'light'){
+        setMode('dark');
+        document.body.style.backgroundColor = '#092a4a';
+     }
+     else{
+        setMode('light');
+        document.body.style.backgroundColor = 'white';
+     }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title ="TextApp" mode={mode} toggleMode={toggleMode} />
+      <TextForm title="Enter the text here:" mode={mode}/>
+    </>
   );
 }
 
